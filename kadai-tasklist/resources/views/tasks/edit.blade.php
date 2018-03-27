9bb79c0e86f481537cb68632dc906e0cf1175971
EC2 Default User <ec2-user@ip-172-31-28-16.us-east-2.compute.internal>
2018-03-27 09:58:12 +0000

    bootstrap design

--- kadai-tasklist/resources/views/tasks/edit.blade.php
+++ kadai-tasklist/resources/views/tasks/edit.blade.php
@@ -4,16 +4,21 @@
 
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
+<div class="row">
+        <div class="col-xs-12　col-sm-offset-2 col-sm-4 col-md-offset-2 col-md-4 col-lg-offset-3 col-lg-3">
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-
+    <div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
         {!! Form::text('status') !!}
-
+    </div>
+        <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
-
+    </div>
         {!! Form::submit('更新') !!}
 
     {!! Form::close() !!}
+        </div>
+    </div>
 
 @endsection
